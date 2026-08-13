968513e21fad1f1035c9f104659a9fa0ee00309f
Gayatrinav <navgharegayatri83@gmail.com>
2026-08-13 17:35:32 +0530

    add 1657_Determine_if_Two_Strings_Are_Close.py

---
+++ Hash map/set/1207_Unique_Number_of_Occurrences.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+class Solution:
+    def uniqueOccurrences(self, arr):
+        freq = Counter(arr)
+
+        return len(freq.values()) == len(set(freq.values()))
